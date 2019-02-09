d47486ce4171265181d625267bc3c4642fa8845b
carlstrand <43237772+carlstrand@users.noreply.github.com>
2019-02-09 15:31:40 +0100

    Update setup.sh

--- env/setup.sh
+++ env/setup.sh
@@ -15,6 +15,12 @@ sudo apt-fast update | sudo apt-fast install hstr
 hstr --show-configuration >> ~/.bashrc && . ~/.bashrc
 
 
+### is.sh  ####
+# https://github.com/qzb/is.sh
+
+sudo sh -c 'cd /usr/local/bin && wget raw.githubusercontent.com/qzb/is.sh/latest/is.sh -O is && chmod +x is'
+
+
 # Allow apt to install system updates automatically every day.
 sudo cat > /etc/apt/apt.conf.d/02periodic <<EOF;
 APT::Periodic::MaxAge "7";
@@ -24,10 +30,10 @@ APT::Periodic::Verbose "0";
 EOF
 
 if [ -f /etc/apt/apt.conf.d/50unattended-upgrades ]; then
-	if ! grep -q "$1" /etc/apt/apt.conf.d/50unattended-upgrades; then
-	sudo sed -i "/Allowed-Origins/a \
-	\"$1\";" /etc/apt/apt.conf.d/50unattended-upgrades
-	fi
+    if ! grep -q "$1" /etc/apt/apt.conf.d/50unattended-upgrades; then
+        sudo sed -i "/Allowed-Origins/a \
+        \"$1\";" /etc/apt/apt.conf.d/50unattended-upgrades
+    fi
 fi
 
 
